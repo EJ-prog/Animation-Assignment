a6ac930d80bb52a5fdcab015b0367f06b3f2cbc5
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2022-01-21 05:01:24 -0800

    Animator and Link class

---
+++ Link.js
@@ -0,0 +1,14 @@
+class Link {
+	constructor(game) {
+		this.game = game;
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./LinkSpriteSheet.png"), 0, 420, 125, 100, 10, 0.5);
+	};
+
+	update() {
+
+	};
+
+	draw(ctx) {
+		this.animator.drawFrame(this.game.clockTick, ctx, 25, 25);
+	};
+}
