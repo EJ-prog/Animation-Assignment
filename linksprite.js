d19770010a1957f4d133fd5fb26712edae59396f
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2022-01-21 15:08:57 -0800

    renamed link to linksprite

--- linksprite.js
+++ linksprite.js
@@ -1,4 +1,4 @@
-class Link {
+class LinkSprite {
 	constructor(game) {
 		this.game = game;
 		this.animator = new Animator(ASSET_MANAGER.getAsset("./LinkSpriteSheet.png"), 0, 0, 125, 100, 10, 0.2);
