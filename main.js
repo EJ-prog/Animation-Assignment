ae85397ce513b189ba5716d0fffd500875bb9cb2
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2022-01-22 12:30:05 -0800

    uncommented section

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.queueDownload("./tilingrocksunlit.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-	//ctx.imageSmoothingEnabled=false;
+	ctx.imageSmoothingEnabled=false;
 
 	gameEngine.addEntity(new LinkSprite(gameEngine));
 	gameEngine.addEntity(new background());
